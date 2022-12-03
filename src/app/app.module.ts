7962e522305539b65f3f80a911801167b0cb569e
Pedro Lima <emailpedrolima@gmail.com>
2022-12-03 13:10:22 -0300

    exercicio 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+import { ModalComponent } from './modal/modal.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
+    ModalComponent,
+    ItemCardComponent
   ],
   imports: [
     BrowserModule
